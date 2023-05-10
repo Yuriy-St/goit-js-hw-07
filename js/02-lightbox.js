7944685fe8b13f3d8ad1fb30baed77cf40c6b18c
Yuriy-St <yuriystaynov@gmail.com>
2023-05-10 20:27:13 +0300

    lightbox page

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,28 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.body.querySelector('.gallery');
+gallery.innerHTML = galleryItems.reduce((htmlGallery, item) => {
+  return (
+    htmlGallery +
+    `
+	<li class="gallery__item">
+   <a class="gallery__link" href="${item.original}">
+      <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+   </a>
+</li>
+`
+  );
+}, '');
+
+// for (const li of gallery.children) {
+//   li.querySelector('a').addEventListener('click', event =>
+//     event.preventDefault()
+//   );
+// }
+
+const lightbox = new SimpleLightbox(`.gallery a`, {
+  captionsData: 'alt',
+  fadeSpeed: 250,
+});
+console.log(gallery);
