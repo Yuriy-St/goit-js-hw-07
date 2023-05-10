eff31217b511880891478b45c76f6c369a82c935
Yuriy-St <yuriystaynov@gmail.com>
2023-05-10 11:35:00 +0300

    gallery 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,37 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
+// import * as basicLightbox from 'basiclightbox'
 
-console.log(galleryItems);
+const instance = basicLightbox.create(`
+    <img src="" width="1280">
+`);
+
+// Change code below this line
+const gallery = document.body.querySelector('.gallery');
+gallery.innerHTML = galleryItems.reduce((htmlGallery, item) => {
+	return htmlGallery + `
+	<li class="gallery__item">
+		<a class="gallery__link" href="">
+			<img
+				class="gallery__image"
+				src="${item.preview}"
+				data-source="${item.original}"
+				alt="${item.description}"
+			/>
+		</a>
+	</li>
+`
+}, '');
+
+for (const li of gallery.children) {
+	li.querySelector('a').addEventListener('click', event => event.preventDefault());
+};
+
+gallery.addEventListener('click', event => {
+	const { target } = event;
+	event.stopPropagation();
+	if (target.nodeName !== 'IMG') return;
+	instance.element().querySelector('img').src = target.dataset.source;
+	instance.show();
+})
+
+console.log(instance.element());
