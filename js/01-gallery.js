80b6697c6a231bee6655f8918d19d04f57bfb017
Yuriy-St <yuriystaynov@gmail.com>
2023-05-10 14:11:13 +0300

    closing lightbox by escape button

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,14 +1,16 @@
 import { galleryItems } from './gallery-items.js';
 // import * as basicLightbox from 'basiclightbox'
 
-const instance = basicLightbox.create(`
-    <img src="" width="1280">
-`);
+const instance = basicLightbox.create(`<img src="" width="1280">`, {
+  onClose: removeLightboxEscapeListener,
+});
 
 // Change code below this line
 const gallery = document.body.querySelector('.gallery');
 gallery.innerHTML = galleryItems.reduce((htmlGallery, item) => {
-	return htmlGallery + `
+  return (
+    htmlGallery +
+    `
 	<li class="gallery__item">
 		<a class="gallery__link" href="">
 			<img
@@ -20,18 +22,36 @@ gallery.innerHTML = galleryItems.reduce((htmlGallery, item) => {
 		</a>
 	</li>
 `
+  );
 }, '');
 
 for (const li of gallery.children) {
-	li.querySelector('a').addEventListener('click', event => event.preventDefault());
-};
+  li.querySelector('a').addEventListener('click', event =>
+    event.preventDefault()
+  );
+}
 
-gallery.addEventListener('click', event => {
-	const { target } = event;
-	event.stopPropagation();
-	if (target.nodeName !== 'IMG') return;
-	instance.element().querySelector('img').src = target.dataset.source;
-	instance.show();
-})
+function showLightbox(event) {
+  const { target } = event;
+  event.stopPropagation();
+  if (target.nodeName !== 'IMG') return;
+  instance.element().querySelector('img').src = target.dataset.source;
+  instance.show();
+  onShowLightbox();
+}
 
-console.log(instance.element());
+function onShowLightbox() {
+  window.addEventListener('keyup', onCloseLightbox);
+}
+
+function onCloseLightbox(event) {
+  if (instance.visible() && event.key === 'Escape') {
+    instance.close();
+  }
+}
+
+function removeLightboxEscapeListener() {
+  window.removeEventListener('keyup', onCloseLightbox);
+}
+
+gallery.addEventListener('click', showLightbox);
