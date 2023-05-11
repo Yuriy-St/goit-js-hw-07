d004eb8958c3088ced75aec9adea22f85fcdf31e
Yuriy-St <yuriystaynov@gmail.com>
2023-05-11 20:45:26 +0300

    flimsy improvements

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -6,23 +6,17 @@ gallery.innerHTML = galleryItems.reduce((htmlGallery, item) => {
   return (
     htmlGallery +
     `
-	<li class="gallery__item">
-   <a class="gallery__link" href="${item.original}">
-      <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
-   </a>
-</li>
-`
+    <li class="gallery__item">
+    <a class="gallery__link" href="${item.original}">
+        <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+    </a>
+    </li>
+    `
   );
 }, '');
 
-// for (const li of gallery.children) {
-//   li.querySelector('a').addEventListener('click', event =>
-//     event.preventDefault()
-//   );
-// }
-
 const lightbox = new SimpleLightbox(`.gallery a`, {
   captionsData: 'alt',
-  fadeSpeed: 250,
+  captionDelay: 250,
 });
 console.log(gallery);
